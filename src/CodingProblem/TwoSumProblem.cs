df213adb7a754a981ea24aba9c7ad4fef40c1bb2
Sanjay Bopche <sanjay.bopche@globallogic.com>
2026-01-21 12:36:03 +0530

    two sum

---
+++ src/CodingProblem/TwoSumProblem.cs
@@ -0,0 +1,22 @@
+﻿namespace CodingProblem
+{
+    internal class TwoSumProblem
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
+
